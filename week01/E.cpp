51d4e19e525f3c784336ac949c20c766c8fb8af2
Sopromath <117065709+Sopromath@users.noreply.github.com>
2024-02-11 13:20:31 +0300

    Update E.cpp

--- week01/E.cpp
+++ week01/E.cpp
@@ -4,36 +4,12 @@ using namespace std;
 
 int main(){
     
-    int s = 0;
+    int a = 1;
     int S = 0;
-    int h = 1;
-    int A[h]= {1};
-    int B[h]= {1};
     
-    while (s == 0){
-        
-        int A[h];
-        for (int i = 0; i<h-1; i++)
-        {
-            B[i] = A[i];
-        }
-        cin>>A[h-1];
-        
-        if(A[h-1] == 0){
-            s = 1;
-            for (int j = 0;j<h;j++)
-            {
-                S = S + A[j];
-            }
-        }
-        
-        int B[h];
-        for (int i = 0; i<h; i++)
-        {
-            B[i] = A[i];
-        }
-        
-        h++;    
+    while(a!=0){
+        cin>>a;
+        S = S+a;
     }
 
     cout<<S;
