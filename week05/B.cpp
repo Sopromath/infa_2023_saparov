ad5dea1ffb3f8fe34d395be16d6ced7ee2074001
Sopromath <117065709+Sopromath@users.noreply.github.com>
2024-02-29 13:41:42 +0300

    Update B.cpp

--- week05/B.cpp
+++ week05/B.cpp
@@ -13,16 +13,18 @@ int main()
     cin >> d;
 
     for(int i = 0; i < d.size(); ++i){
-        for(int j = 0; j < 26; ++j){
-            if(d[i] == s[j]){
-                d[i] = s[(j-k)%26];
-                break;
-            }
+        if(d[i] > 'a' + 25){
+            break;
+        }
+        if(d[i] < 'a'){
+            break;
+        }
+        else{
+            d[i] = 'a' +(d[i] - 'a' - k + 26)%26;
         }
     }
 
-    cout << d;
-
+    cout  <<d;
 
     return 0;
 }
